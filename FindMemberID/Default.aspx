c3b3d7712d3d17f74416c3eafeeeee51b97920a7
Steve Bruell <bruell@cs.uiowa.edu>
2018-10-07 14:34:20 -0500

    Changed Default.aspx

--- FindMemberID/Default.aspx
+++ FindMemberID/Default.aspx
@@ -1,8 +1,37 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FindMemberID._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <script type="text/javascript">
+    function RefreshUpdatePanel() {
+        __doPostBack('<%= filterTextBox.ClientID %>', '');
+        };
+    </script>
 
-    <div class="jumbotron">
+    <br />
+
+    <div>
+        <asp:Label Text="Filter by Last Name: " runat="server" />
+        <asp:TextBox ID="filterTextBox" onkeyup="RefreshUpdatePanel();" onfocus="this.setSelectionRange(this.value.length,  this.value.length);" OnTextChanged="FilterTextBox_TextChanged" runat="server" />
+    </div>
+
+    <br />
+
+    <div>
+        <asp:GridView ID="usersGridView" AutoGenerateColumns="false" runat="server">
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" />
+                <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="userSelected" Text="Select" CommandArgument='<%# Eval("ID") %>' OnClick="UserSelected_Click" runat="server" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+
+    <%--<div class="jumbotron">
         <h1>ASP.NET</h1>
         <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
         <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
@@ -37,6 +66,6 @@
                 <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
             </p>
         </div>
-    </div>
+    </div>--%>
 
 </asp:Content>
--- FindMemberID/Default.aspx.cs
+++ FindMemberID/Default.aspx.cs
@@ -1,17 +1,102 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : FindMemberID
+// Author           : Steve Bruell
+// Created          : 10-02-2018
+//
+// Last Modified By : Steve Bruell
+// Last Modified On : 10-03-2018
+// ***********************************************************************
+// <copyright file="Default.aspx.cs" company="SCB">
+//     SCB. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Globalization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using StpcDB;
+using StpcModels;
 
 namespace FindMemberID
 {
     public partial class _Default : Page
     {
+        private List<Member> allMembers;
+        private List<Member> memberListToDisplay;
+
+        public _Default()
+        {
+            DB.Instance.ConnectionString = ConfigurationManager.ConnectionStrings["StpcDB"].ConnectionString + "3";
+
+            this.allMembers = this.memberListToDisplay = this.GetAll();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsCallback)
+            {
+                if (this.filterTextBox.Text != null)
+                {
+                    this.UpdateDisplayedMembersByLastName(this.filterTextBox.Text);
+                }
 
+                this.usersGridView.DataSource = this.DisplayMembers;
+
+                this.usersGridView.DataBind();
+            }
+        }
+
+        public List<Member> DisplayMembers
+        {
+            get { return this.memberListToDisplay; }
+
+            set { this.memberListToDisplay = value; }
+        }
+
+        public List<Member> GetAll()
+        {
+            return new List<Member>(DB.Instance.GetAllMembers<SqlMember>());
+        }
+
+        protected void UserSelected_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32((sender as LinkButton).CommandArgument, CultureInfo.CurrentCulture);
+
+            string displayText = "Nothing selected";
+
+            foreach (Member member in this.DisplayMembers)
+            {
+                if (member.ID == id)
+                {
+                    displayText = id + " " + member.LastName + ", " + member.FirstName;
+                    break;
+                }
+            }
+
+            Page.ClientScript.RegisterStartupScript(
+                   Page.GetType(),
+                   "MessageBox",
+                   "<script language='javascript'>prompt('Press ctrl+C to select text in box below', '" + displayText + "');</script>");
+        }
+
+        protected void FilterTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.UpdateDisplayedMembersByLastName((sender as TextBox).Text);
+
+            Page.SetFocus(filterTextBox);
+        }
+
+        private bool ShouldMemberBeIncluded(string nameOrID, string filter)
+        {
+            return nameOrID.ToLower(CultureInfo.CurrentCulture).StartsWith(filter.ToLower(CultureInfo.CurrentCulture), StringComparison.CurrentCulture);
+        }
+
+        private void UpdateDisplayedMembersByLastName(string filter)
+        {
+            this.DisplayMembers = filter != null && string.IsNullOrEmpty(filter) ? this.allMembers : this.allMembers.FindAll((member) => this.ShouldMemberBeIncluded(member.LastName, filter));
         }
     }
 }
